24a633a5f4c17f9c1b543cb3c7118e48b71353c4
Mathieu NICOLAS <mathieu.nicolas@me.com>
2023-04-11 03:47:16 +0200

    ajout du module content-assets

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,25 +1,32 @@
 export default defineNuxtConfig({
   extends: '@nuxt-themes/docus',
-  modules: ['@nuxt/content'],
+  modules: [
+    'nuxt-content-assets', // make sure to add before content!
+    '@nuxt/content'
+  ],
   css: ['@/assets/main.css'],
   content: {
-    //ignores: ['.+\.(?:pdf|jpg|png|epub|excalidraw)'],
-    sources: {
-      gh: {
-        driver: 'github',
-        repo: 'mathieunicolas/alloe',
-        prefix: '/'
-      }
-    },
+    // sources: {
+    //   gh: {
+    //     driver: 'github',
+    //     repo: 'mathieunicolas/alloe',
+    //     dir: '/',
+    //     prefix: '/'
+    //   }
+    // },
     navigation: {
       fields: ['auteur']
     }
   },
-  nitro: {
-    prerender: {
-      routes: [
-          '/alloe18'
-      ]
-    }
-  }
+  'content-assets': {
+    output: '/[folder]/[file]',
+    additionalExtensions: 'epub',
+  },
+  // nitro: {
+  //   prerender: {
+  //     routes: [
+  //         '/alloe18'
+  //     ]
+  //   }
+  // }
 })
