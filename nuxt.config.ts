20877cdb3a933cf68f432e3abef2f35715ffa93e
Mathieu NICOLAS <mathieu.nicolas@me.com>
2023-07-04 20:10:47 +0200

    update

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -46,7 +46,7 @@ export default defineNuxtConfig({
     workbox: {
       navigateFallback: undefined,
       globPatterns: ['**/*.{js,css,html,png,svg,ico,json}', 'api/**', 'api/*'],
-      ignoreURLParametersMatching: [/^\/api\//],
+      ignoreURLParametersMatching: [/^api\//],
     },
     client: {
       installPrompt: true,
