bc2212b03db1103d03ee7702fd495f521b7e67f5
Mathieu NICOLAS <mathieu.nicolas@me.com>
2023-10-18 18:08:54 +0200

    update

--- components/content/gallery.vue
+++ components/content/gallery.vue
@@ -1,10 +1,10 @@
 <script setup>
 const covers = [
-  'alloe18/3arR5cP.jpg',
-  'alloe19/JzU6Iyo.jpg',
-  'alloe20/Zx13oLOe.png',
-  'alloe21/468zwUH1.png',
-  'alloe22/oX3Ru33f.jpg',
+  { cover: 'alloe18/3arR5cP.jpg', title: 'édition 2018', src: '/alloe18' },
+  { cover: 'alloe19/JzU6Iyo.jpg', title: 'édition 2019', src: '/alloe19' },
+  { cover: 'alloe20/Zx13oLOe.png', title: 'édition 2020', src: '/alloe20' },
+  { cover: 'alloe21/468zwUH1.png', title: 'édition 2021', src: '/alloe21' },
+  { cover: 'alloe22/oX3Ru33f.jpg', title: 'édition 2022', src: '/alloe22' },
 ]
 
 const inc = ref(0)
@@ -16,7 +16,12 @@ setInterval(() => {
 
 <template>
   <Transition name="fade" mode="out-in">
-    <img :key="inc" :src="'https://raw.githubusercontent.com/mathieunicolas/alloe/main/'+covers[inc]">
+  <figure>
+  <NuxtLink :to="covers[inc].src">
+    <img :key="inc" :src="'https://raw.githubusercontent.com/mathieunicolas/alloe/main/'+covers[inc].cover">
+  </NuxtLink>
+    <figcaption style="text-align: center;">{{ covers[inc].title }}</figcaption>
+  </figure>
   </Transition>
 </template>
 
