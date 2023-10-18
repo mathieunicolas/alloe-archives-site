fb62879f286dbaa6ff0d10fb393651c7528c0292
Mathieu NICOLAS <mathieu.nicolas@me.com>
2023-10-18 18:23:16 +0200

    update

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -26,7 +26,7 @@ export default defineNuxtConfig({
       github: {
         prefix: '/', // Prefix for routes used to query contents
         driver: 'github', // Driver used to fetch contents (view unstorage documentation)
-        repo: "mathieunicolas/alloe",
+        repo: "mathieunicolas/alloe-archives",
         branch: "main",
         dir: "/", // Directory where contents are located. It could be a subdirectory of the repository.
         // Imagine you have a blog inside your content folder. You can set this option to `content/blog` with the prefix option to `/blog` to avoid conflicts with local files.
